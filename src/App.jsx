706f49865804856b8a8a5394c33b6cadabcda6f5
martemoslet <martemoslet@gmail.com>
2023-10-06 13:50:11 +0200

    Adds routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,18 @@
 import "./App.css";
+import { Routes, Route } from "react-router-dom";
+import Layout from "./components/Layout";
+import Home from "./pages/Home";
+import Contact from "./pages/Contact";
+import About from "./pages/About";
 
 export default function App() {
-  return <div>Component</div>;
+  return (
+    <Routes>
+      <Route path="/" element={<Layout />}>
+        <Route index element={<Home />} />
+        <Route path="contact" element={<Contact />} />
+        <Route path="about" element={<About />} />
+      </Route>
+    </Routes>
+  );
 }
